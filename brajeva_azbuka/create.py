966a2ccff785a2340d68e5bcb194c146fe0c59db
mvujas <milos.vujasinovic.98@gmail.com>
2017-01-20 22:31:46 +0100

    Minnor changes

--- brajeva_azbuka/create.py
+++ brajeva_azbuka/create.py
@@ -1,10 +1,10 @@
-from models import Brain
+from models import NeuralNetwork
 
 def main():
-	net = Brain(6, 15, 2, 3, momentum=0.99)
+	net = NeuralNetwork(6, 15, 2, 3, momentum=0.99)
 	net.load_dataset_from_file("trainingData.txt")
 	net.train(maxEpochs=30000, desiredError=0.0000001)
-	net.save_net_to_file("brajeva_azbuka_15_2.nnet")
+	net.save_net_to_file("brajeva_azbuka.nnet")
 
 if __name__ == "__main__":
 	main()
