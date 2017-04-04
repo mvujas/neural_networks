f52a70c1a7f4aeb0fd69e4c915316e249cf683f7
Milos <milos.vujasinovic.98@gmail.com>
2017-04-04 18:34:17 +0200

    minnor changes

--- brajeva_azbuka/models.py
+++ brajeva_azbuka/models.py
@@ -26,17 +26,10 @@ def decodeRet(*arrgs):
 		return signs[int(round(lista[2] * 100 / 3)) - 1]
 
 class NeuralNetwork:
-	def __init__(self, *layers, **options):
-		opt = {'momentum': 0.99}
-		for key in options:
-			if key not in list(opt.keys()):
-				raise NetworkError('Brain constructor unknown option: %s' % key)
-			opt[key] = options[key]
-
+	def __init__(self, *layers):
 		self.__net = buildNetwork(*layers, hiddenclass=SigmoidLayer, outclass=SigmoidLayer, bias=True)
 		self.__data = SupervisedDataSet(layers[0], layers[-1])
-		self.__momentum = opt['momentum']
-		self.__trainer = BackpropTrainer(self.__net, self.__data, momentum=self.__momentum)
+		self.__trainer = BackpropTrainer(self.__net, self.__data, momentum=0.99)
 		self.__layers = layers
 
 	def load_dataset_from_file(self, f):
@@ -47,17 +40,13 @@ class NeuralNetwork:
 		    outdata = tuple(data[self.__layers[0]:])
 		    self.__data.addSample(indata,outdata)
 		infile.close()
-		self.__trainer = BackpropTrainer(self.__net, self.__data, momentum=self.__momentum)
-
-	def write_dataset(self):
-		for inpt, target in self.__data:
-			print("(", inpt, " ", target, ")")
+		self.__trainer = BackpropTrainer(self.__net, self.__data, momentum=0.99)
 
 	def load_net_from_file(self, f):
 		infile = open(f, "r")
 		self.__net = pickle.load(infile)
 		infile.close()
-		self.__trainer = BackpropTrainer(self.__net, self.__data, momentum=self.__momentum)
+		self.__trainer = BackpropTrainer(self.__net, self.__data, momentum=0.99)
 
 	def get_acc(self):
 		if len(self.__data) != 0:
